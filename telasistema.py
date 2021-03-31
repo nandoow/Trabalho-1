0846bb6a90cfb8ae8b4dfbc35e86c3561f7c5896
Mitsz <63883691+Mitsz@users.noreply.github.com>
2021-03-31 20:53:30 -0300

    Create telasistema.py

---
+++ telasistema.py
@@ -0,0 +1,21 @@
+
+
+
+
+class TelaSistema():
+    def __init__(self):
+        pass
+
+
+    def tela_opcoes(self):
+        print('-----POSTO DE VACINAÇÃO-----')
+        print('ESCOLHA O NÚMERO DA OPERAÇÃO QUE DESEJA REALIZAR')
+        print('1 - Cadastrar posto de vacinação')
+        print('2 - Gerenciar posto de vacinação')
+        print('3 - Excluir posto de vacinação')
+
+        opcao = int(input('Escolha a opção:'))
+        return opcao 
+
+
+    
